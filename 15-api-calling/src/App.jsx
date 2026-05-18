2eeb4152a5ad3717aab4d17aebd7b2655205f4ab
Raj Singh <rajsinghrk7786@gmail.com>
2026-05-19 00:10:28 +0530

    api-calling

---
+++ 15-api-calling/src/App.jsx
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { useState } from "react";
+
+const App = () => {
+
+  const [data,setData] = useState([]);
+  const getData = async () => {
+    const response = await axios.get("https://picsum.photos/v2/list");
+    setData(response.data);
+  };
+
+  return (
+    <div>
+      <button onClick={getData}>Get Data</button>
+      <div>
+        {data.map(function(elem,idx){
+          return <h1>Hello, {elem.author}</h1>
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default App;
