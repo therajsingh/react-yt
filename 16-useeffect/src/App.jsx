5ce5b095dc791eb9473a04ef91306e55ba903ad9
Raj Singh <rajsinghrk7786@gmail.com>
2026-05-19 15:09:39 +0530

    done-useeffect

---
+++ 16-useeffect/src/App.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+
+const App = () => {
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
+
+  function aChanging() {
+    console.log(`A ki value change ho gyi`);
+  }
+  function bChanging() {
+    console.log(`B ki value change ho gyi`);
+  }
+
+  useEffect(() => {
+    aChanging();
+  }, [a]);
+  useEffect(() => {
+    bChanging();
+  }, [b]);
+
+  return (
+    <div>
+      <h1>A is {a}</h1>
+      <h1>B is {b}</h1>
+      <button
+        onClick={() => {
+          setA(a + 1);
+        }}
+      >
+        Change A
+      </button>
+      <button
+        onClick={() => {
+          setB(b - 1);
+        }}
+      >
+        Change B
+      </button>
+    </div>
+  );
+};
+
+export default App;
